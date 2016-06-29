310b24aeb5546803da1bcabc5a55a6396b884b17
jinyoung7950 <jinyoung@lectopia.com>
2016-06-29 15:56:38 +0900

    add
    add

--- lectopiaBoard/src/com/lectopia/web/BoardServlet.java
+++ lectopiaBoard/src/com/lectopia/web/BoardServlet.java
@@ -20,6 +20,7 @@ public class BoardServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String url=null;
+		System.out.println("add");
 		switch(request.getServletPath()){
 		case "/index.do":
 			url="pages/list.jsp";
